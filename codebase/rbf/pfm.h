f66105aaf8e308cf53f7a73bd7ee969e985202f5
Jianfei Hu <hujianfei258@gmail.com>
2014-01-16 09:20:36 -0800

    finish functions in pfm.cc

--- codebase/rbf/pfm.h
+++ codebase/rbf/pfm.h
@@ -4,6 +4,13 @@
 typedef int RC;
 typedef unsigned PageNum;
 
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <string>
+#include <cstdio>
+using namespace std;
+
 #define PAGE_SIZE 4096
 
 class FileHandle;
@@ -19,12 +26,16 @@ public:
     RC openFile      (const char *fileName, FileHandle &fileHandle); // Open a file
     RC closeFile     (FileHandle &fileHandle);                       // Close a file
 
+private:
+	bool isFileExit(const char *filename);
+
 protected:
     PagedFileManager();                                   // Constructor
     ~PagedFileManager();                                  // Destructor
 
 private:
     static PagedFileManager *_pf_manager;
+	map<string, int> _pf_open_count; //record the open count of certain file
 };
 
 
@@ -38,6 +49,12 @@ public:
     RC writePage(PageNum pageNum, const void *data);                    // Write a specific page
     RC appendPage(const void *data);                                    // Append a specific page
     unsigned getNumberOfPages();                                        // Get the number of pages in the file
+
+	//associated file
+	string _fh_name; // file name
+	fstream *_fh_file; // fstream
+
+private:
 };
 
 #endif
